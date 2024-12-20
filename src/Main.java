597e3c84d41cb6c7cb9657bca61b37a3200b1667
smithbwlhs <smithb@wlwv.k12.or.us>
2024-12-20 07:02:35 -0800

    moved getBirthday to own static method

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter birthdate in yyyy-mm-dd form: ");
+        LocalDate customerBirthdate = getBirthdate();
+        boolean alcoholAccess = canAlcohol(customerBirthdate);
+        System.out.println(alcoholAccess);
+    }
+
+    public static String getName(){
+
+        return null;
+    }
+
+    public static LocalDate getBirthdate(){
         int year, month, day;
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter birthdate in yyyy-mm-dd form: ");
-
         try{
             String birthdate = scan.nextLine();
             year = Integer.parseInt(birthdate.substring(0,4));
             month = Integer.parseInt(birthdate.substring(5,7));
             day = Integer.parseInt(birthdate.substring(8));
             LocalDate customerBirthdate = LocalDate.of(year, month, day);
-            boolean alcoholAccess = canAlcohol(customerBirthdate);
-            System.out.println(alcoholAccess);
+            scan.close();
+            return customerBirthdate;
         } catch (NumberFormatException e) {
             System.out.print("Enter birthdate in yyyy-mm-dd form: ");;
         }
-
-
+        return(LocalDate.MAX);
     }
 
     public static boolean canAlcohol(LocalDate birthdate){
